1e73eb23397a5f7263be8409bb6ff45e74636fb5
Altynbek312 <altyn_312@mail.ru>
2023-08-22 20:31:18 +0600

    TASK-3 (Home page markup) - feat: add home page view         modified:   .gitignore         new file:   src/App/views/index.php         new file:   src/App/views/partials/_footer.php         new file:   src/App/views/partials/_header.php

---
+++ src/App/views/index.php
@@ -0,0 +1,336 @@
+<?php include $this->resolve("partials/_header.php"); ?>
+
+
+    <main>
+      <div class="wrapper">
+        <div class="container">
+          <div class="main__article">
+            <a
+              href="https://www.positive.news/society/electric-van-summer-holidays-with-children/"
+              class="card__image__link col--7 col--12--bp3"
+            >
+              <img
+                src="https://www.positive.news/wp-content/uploads/2023/07/1-4-1320x880-c-center.jpg"
+                class="card__image main__article--img img100"
+                alt="Image for How an electric van changed our summer holidays forever"
+              />
+            </a>
+
+            <div class="card__content col--5--last col--12--bp3">
+              <a
+                href="https://www.positive.news/society/electric-van-summer-holidays-with-children/"
+                class="card__title h2"
+                >How an electric van changed our summer holidays forever</a
+              >
+              <p>
+                An electric Nissan has put a spark in summer holidays for one
+                cash-strapped family, who share their tips for travelling by van
+              </p>
+              <a
+                href="https://www.positive.news/category/environment/"
+                class="card__category"
+                >Environment</a
+              >
+              <a
+                href="https://www.positive.news/category/lifestyle/"
+                class="card__category"
+                >Lifestyle</a
+              >
+              <a
+                href="https://www.positive.news/category/society/"
+                class="card__category"
+                >Society</a
+              >
+              <a
+                href="https://www.positive.news/category/lifestyle/travel/"
+                class="card__category"
+                >Travel</a
+              >
+            </div>
+          </div>
+          <div class="latest__articles cols--3--2--2">
+            <div class="column card">
+              <a
+                href="https://www.positive.news/lifestyle/how-to-live-longer-eight-habits-that-could-add-years-to-your-life/"
+                class="card__image__link"
+              >
+                <img
+                  src="./img/1200-×-800px-58-740x492-c-center.jpg"
+                  class="card__image main__article--img img100"
+                  alt="Image for How to live longer: eight habits that could add years to your life"
+                />
+              </a>
+
+              <div class="card__content">
+                <a
+                  href="https://www.positive.news/lifestyle/how-to-live-longer-eight-habits-that-could-add-years-to-your-life/"
+                  class="card__title h3"
+                  >How to live longer: eight habits that could add years to your
+                  life</a
+                >
+                <span class="card__text">
+                  These lifestyle habits could extend your life by decades,
+                  according to new research. Even doing a few could help
+                </span>
+                <a
+                  href="https://www.positive.news/category/lifestyle/body-mind/"
+                  class="card__category"
+                  >Body &amp; Mind</a
+                >
+                <a
+                  href="https://www.positive.news/category/lifestyle/health/"
+                  class="card__category"
+                  >Health</a
+                >
+                <a
+                  href="https://www.positive.news/category/lifestyle/"
+                  class="card__category"
+                  >Lifestyle</a
+                >
+                <a
+                  href="https://www.positive.news/category/lifestyle/wellbeing/"
+                  class="card__category"
+                  >Wellbeing</a
+                >
+              </div>
+            </div>
+
+            <div class="column card">
+              <a
+                href="https://www.positive.news/lifestyle/zimbabwe-friendship-benches-are-coming-to-a-city-near-you/"
+                class="card__image__link"
+              >
+                <img
+                  src="./img/1200-×-800px-41-740x492-c-center.jpg"
+                  class="card__image main__article--img img100"
+                  alt="Image for Zimbabwe’s therapeutic ‘friendship benches’, coming to a city near you"
+                />
+              </a>
+
+              <div class="card__content">
+                <a
+                  href="https://www.positive.news/lifestyle/zimbabwe-friendship-benches-are-coming-to-a-city-near-you/"
+                  class="card__title h3"
+                  >Zimbabwe’s therapeutic ‘friendship benches’, coming to a city
+                  near you</a
+                >
+                <span class="card__text">
+                  A Zimbabwean project that employs grandmothers to deliver
+                  therapy is being rolled out globally. Up next? London
+                </span>
+                <a
+                  href="https://www.positive.news/category/lifestyle/health/"
+                  class="card__category"
+                  >Health</a
+                >
+                <a
+                  href="https://www.positive.news/category/lifestyle/"
+                  class="card__category"
+                  >Lifestyle</a
+                >
+                <a
+                  href="https://www.positive.news/category/society/"
+                  class="card__category"
+                  >Society</a
+                >
+                <a
+                  href="https://www.positive.news/category/lifestyle/wellbeing/"
+                  class="card__category"
+                  >Wellbeing</a
+                >
+                <a
+                  href="https://www.positive.news/category/world/"
+                  class="card__category"
+                  >World</a
+                >
+              </div>
+            </div>
+
+            <div class="column card">
+              <a
+                href="https://www.positive.news/lifestyle/zimbabwe-friendship-benches-are-coming-to-a-city-near-you/"
+                class="card__image__link"
+              >
+                <img
+                  src="./img/1200-×-800px-41-740x492-c-center.jpg"
+                  class="card__image main__article--img img100"
+                  alt="Image for Zimbabwe’s therapeutic ‘friendship benches’, coming to a city near you"
+                />
+              </a>
+
+              <div class="card__content">
+                <a
+                  href="https://www.positive.news/lifestyle/zimbabwe-friendship-benches-are-coming-to-a-city-near-you/"
+                  class="card__title h3"
+                  >Zimbabwe’s therapeutic ‘friendship benches’, coming to a city
+                  near you</a
+                >
+                <span class="card__text">
+                  A Zimbabwean project that employs grandmothers to deliver
+                  therapy is being rolled out globally. Up next? London
+                </span>
+                <a
+                  href="https://www.positive.news/category/lifestyle/health/"
+                  class="card__category"
+                  >Health</a
+                >
+                <a
+                  href="https://www.positive.news/category/lifestyle/"
+                  class="card__category"
+                  >Lifestyle</a
+                >
+                <a
+                  href="https://www.positive.news/category/society/"
+                  class="card__category"
+                  >Society</a
+                >
+                <a
+                  href="https://www.positive.news/category/lifestyle/wellbeing/"
+                  class="card__category"
+                  >Wellbeing</a
+                >
+                <a
+                  href="https://www.positive.news/category/world/"
+                  class="card__category"
+                  >World</a
+                >
+              </div>
+            </div>
+
+            <div class="column card">
+              <a
+                href="https://www.positive.news/environment/spains-latest-weapon-against-wildfires-wild-horses/"
+                class="card__image__link"
+              >
+                <img
+                  src="./img/iStock-873336362-min-e1690293152202-740x492-c-center.jpg"
+                  class="card__image main__article--img img100"
+                  alt="Image for Spain’s latest weapon against wildfires? Wild horses"
+                />
+              </a>
+
+              <div class="card__content">
+                <a
+                  href="https://www.positive.news/environment/spains-latest-weapon-against-wildfires-wild-horses/"
+                  class="card__title h3"
+                  >Spain’s latest weapon against wildfires? Wild horses</a
+                >
+                <span class="card__text">
+                  Horses are the latest line of defence against wildfires in
+                  Spain. The animals could bring many other benefits besides
+                </span>
+                <a
+                  href="https://www.positive.news/category/environment/conservation/"
+                  class="card__category"
+                  >Conservation</a
+                >
+                <a
+                  href="https://www.positive.news/category/environment/"
+                  class="card__category"
+                  >Environment</a
+                >
+                <a
+                  href="https://www.positive.news/category/world/"
+                  class="card__category"
+                  >World</a
+                >
+              </div>
+            </div>
+
+            <div class="column card">
+              <a
+                href="https://www.positive.news/lifestyle/meet-the-young-graduate-who-decided-to-live-in-a-nursing-home/"
+                class="card__image__link"
+              >
+                <img
+                  src="./img/1-2-740x492-c-center.jpg"
+                  class="card__image main__article--img img100"
+                  alt="Image for Meet the young graduate who lives in a nursing home"
+                />
+              </a>
+
+              <div class="card__content">
+                <a
+                  href="https://www.positive.news/lifestyle/meet-the-young-graduate-who-decided-to-live-in-a-nursing-home/"
+                  class="card__title h3"
+                  >Meet the young graduate who lives in a nursing home</a
+                >
+                <span class="card__text">
+                  Teun Toebes, 24, willingly moved into a care home three years
+                  ago. It changed his life, and the lives of his housemates
+                </span>
+                <a
+                  href="https://www.positive.news/category/lifestyle/"
+                  class="card__category"
+                  >Lifestyle</a
+                >
+                <a
+                  href="https://www.positive.news/category/society/"
+                  class="card__category"
+                  >Society</a
+                >
+                <a
+                  href="https://www.positive.news/category/lifestyle/wellbeing/"
+                  class="card__category"
+                  >Wellbeing</a
+                >
+              </div>
+            </div>
+
+            <div class="column card">
+              <a
+                href="https://www.positive.news/lifestyle/zimbabwe-friendship-benches-are-coming-to-a-city-near-you/"
+                class="card__image__link"
+              >
+                <img
+                  src="./img/1200-×-800px-41-740x492-c-center.jpg"
+                  class="card__image main__article--img img100"
+                  alt="Image for Zimbabwe’s therapeutic ‘friendship benches’, coming to a city near you"
+                />
+              </a>
+
+              <div class="card__content">
+                <a
+                  href="https://www.positive.news/lifestyle/zimbabwe-friendship-benches-are-coming-to-a-city-near-you/"
+                  class="card__title h3"
+                  >Zimbabwe’s therapeutic ‘friendship benches’, coming to a city
+                  near you</a
+                >
+                <span class="card__text">
+                  A Zimbabwean project that employs grandmothers to deliver
+                  therapy is being rolled out globally. Up next? London
+                </span>
+                <a
+                  href="https://www.positive.news/category/lifestyle/health/"
+                  class="card__category"
+                  >Health</a
+                >
+                <a
+                  href="https://www.positive.news/category/lifestyle/"
+                  class="card__category"
+                  >Lifestyle</a
+                >
+                <a
+                  href="https://www.positive.news/category/society/"
+                  class="card__category"
+                  >Society</a
+                >
+                <a
+                  href="https://www.positive.news/category/lifestyle/wellbeing/"
+                  class="card__category"
+                  >Wellbeing</a
+                >
+                <a
+                  href="https://www.positive.news/category/world/"
+                  class="card__category"
+                  >World</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+
+
+<?php include $this->resolve("partials/_footer.php"); ?>
