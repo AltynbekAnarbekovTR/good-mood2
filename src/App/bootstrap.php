ce18611586b71ac6114a18151848323c118571e0
Altynbek312 <altyn_312@mail.ru>
2023-08-24 23:48:03 +0600

    TASK-2 (Project structure) - feat: apply middleware and add routes and add redirect function         modified:   src/App/bootstrap.php         modified:   src/App/functions.php

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -7,12 +7,28 @@ if (!defined('AUTOLOADER_INITIALIZED')) {
 }
 
 use Framework\App;
-use App\Controllers\HomeController;
+use App\Controllers\{
+  HomeController,
+  AboutController,
+  AuthController
+};
+use App\Middleware\{
+  TemplateDataMiddleware,
+  ValidationExceptionMiddleware,
+  SessionMiddleware,
+  FlashMiddleware
+};
 
 $app = new App(Paths::SOURCE . "App/container-definitions.php");
 
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
+$app->get('/register', [AuthController::class, 'registerView']);
+$app->post('/register', [AuthController::class, 'register']);
 
 $app->addMiddleware(TemplateDataMiddleware::class);
+$app->addMiddleware(ValidationExceptionMiddleware::class);
+$app->addMiddleware(FlashMiddleware::class);
+$app->addMiddleware(SessionMiddleware::class);
 
 return $app;
