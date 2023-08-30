2f24865e6aa5d9abc16d414f0658dc6aef40b256
Altynbek312 <altyn_312@mail.ru>
2023-08-31 01:28:09 +0600

    TASK-9 (Authorization) - feat: apply middleware and paths         modified:   Config/Paths.php         modified:   bootstrap.php         modified:   ../Framework/App.php         modified:   ../Framework/Container.php         modified:   ../Framework/Database.php         modified:   ../Framework/Router.php

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -12,20 +12,29 @@ use App\Controllers\{
   AboutController,
   AuthController
 };
+use App\Middleware\{AuthRequiredMiddleware, GuestOnlyMiddleware};
 use App\Middleware\{
   TemplateDataMiddleware,
   ValidationExceptionMiddleware,
   SessionMiddleware,
-  FlashMiddleware
+  FlashMiddleware,
+  CsrfTokenMiddleware,
+  CsrfGuardMiddleware
 };
+
 
 $app = new App(Paths::SOURCE . "App/container-definitions.php");
 
-$app->get('/', [HomeController::class, 'home']);
-$app->get('/about', [AboutController::class, 'about']);
-$app->get('/register', [AuthController::class, 'registerView']);
-$app->post('/register', [AuthController::class, 'register']);
+  $app->get('/', [HomeController::class, 'home'])->add(AuthRequiredMiddleware::class);
+  $app->get('/about', [AboutController::class, 'about']);
+  $app->get('/register', [AuthController::class, 'registerView'])->add(GuestOnlyMiddleware::class);
+  $app->post('/register', [AuthController::class, 'register'])->add(GuestOnlyMiddleware::class);
+  $app->get('/login', [AuthController::class, 'loginView'])->add(GuestOnlyMiddleware::class);
+  $app->post('/login', [AuthController::class, 'login'])->add(GuestOnlyMiddleware::class);
+  $app->get('/logout', [AuthController::class, 'logout'])->add(AuthRequiredMiddleware::class);
 
+$app->addMiddleware(CsrfGuardMiddleware::class);
+$app->addMiddleware(CsrfTokenMiddleware::class);
 $app->addMiddleware(TemplateDataMiddleware::class);
 $app->addMiddleware(ValidationExceptionMiddleware::class);
 $app->addMiddleware(FlashMiddleware::class);
