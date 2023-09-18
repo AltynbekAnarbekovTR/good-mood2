ea65dcc1980f7315ec50989afaabae909499c1ea
Altynbek312 <altyn_312@mail.ru>
2023-09-18 16:20:14 +0600

    TASK-2 (Project structure) - refactor: change database dump         modified:   database.sql

--- database.sql
+++ database.sql
@@ -1,9 +1,178 @@
-CREATE TABLE IF NOT EXISTS users(
-  id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  email varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY(id),
-  UNIQUE KEY(email)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Сен 18 2023 г., 12:18
+-- Версия сервера: 10.4.28-MariaDB
+-- Версия PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `good_mood2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` bigint(20) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `article_text` varchar(1000) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `original_filename` varchar(255) NOT NULL,
+  `storage_filename` varchar(255) NOT NULL,
+  `media_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `article_images`
+--
+
+CREATE TABLE `article_images` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `original_filename` varchar(255) NOT NULL,
+  `storage_filename` varchar(255) NOT NULL,
+  `media_type` varchar(255) NOT NULL,
+  `article_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `auth_codes`
+--
+
+CREATE TABLE `auth_codes` (
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `code` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `auth_codes`
+--
+
+INSERT INTO `auth_codes` (`user_id`, `email`, `code`) VALUES
+(30, 'altyn_312@mail.ru', '2c7bec11873b60c530734fa328231d66');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(20) NOT NULL DEFAULT 'user',
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `email_verified` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `role`, `created_at`, `email_verified`) VALUES
+(30, 'altyn_312@mail.ru', '$2y$12$8nYBQE9rxRZtSN9eMu9u2.NOS134YJSy7oq3CIFNNhd8UUvdc0R3K', 'admin', '2023-09-18 16:11:44', 1);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `articles_ibfk_1` (`user_id`);
+
+--
+-- Индексы таблицы `article_images`
+--
+ALTER TABLE `article_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `transaction_id` (`article_id`);
+
+--
+-- Индексы таблицы `auth_codes`
+--
+ALTER TABLE `auth_codes`
+  ADD KEY `email` (`email`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+
+--
+-- AUTO_INCREMENT для таблицы `article_images`
+--
+ALTER TABLE `article_images`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `articles`
+--
+ALTER TABLE `articles`
+  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `article_images`
+--
+ALTER TABLE `article_images`
+  ADD CONSTRAINT `article_images_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `auth_codes`
+--
+ALTER TABLE `auth_codes`
+  ADD CONSTRAINT `auth_codes_ibfk_1` FOREIGN KEY (`email`) REFERENCES `users` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `auth_codes_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
