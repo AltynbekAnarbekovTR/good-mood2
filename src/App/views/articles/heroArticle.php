bf9d1402f45e02379a8f13928963f28bf50edad2
Altynbek312 <altyn_312@mail.ru>
2023-10-14 15:35:55 +0600

    TASK-26 (Refactoring) - refactoring: change hero article styles         modified:   public/assets/main.css         modified:   src/App/views/articles/heroArticle.php

--- src/App/views/articles/heroArticle.php
+++ src/App/views/articles/heroArticle.php
@@ -1,9 +1,9 @@
 <div class="main__article">
   <a href="/article/<?php echo $mainArticle->getId(); ?>"
-     class="col--7 col--12--bp3 flex justify-center">
+     class="">
       <img class="main__article--img" src="data:image/png;base64,<?php echo $mainArticleImage ?>" alt="Main article">
   </a>
-  <div class="card__content col--5--last col--12--bp3">
+  <div class="card__content">
     <a href="https://www.positive.news/society/electric-van-summer-holidays-with-children/"
        class="card__title h2">
       <?php echo $mainArticle->getTitle(); ?>
